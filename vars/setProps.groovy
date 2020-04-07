2b63020d725b1fb9671741cf4ef03ebd0d410350
Teemu Suvanto <teemu.suvanto@gmail.com>
2020-04-07 14:00:36 +0200

    Create setProps.groovy

---
+++ vars/setProps.groovy
@@ -0,0 +1,24 @@
+#!/usr/bin/env groovy
+
+def call(Map parameters) {
+  List options = []
+
+  if (parameters.extra) {
+      options += parameters.extra
+  }
+
+  options.add([$class: 'BuildDiscarderProperty',
+             strategy: [
+               $class: 'LogRotator', 
+               daysToKeepStr: '90',
+               numToKeepStr: '50',
+               artifactDaysToKeepStr: '90',
+               artifactNumToKeepStr: '50'
+  ]])
+
+  properties(options)
+}
+
+def call(List extra = []) {
+    call(extra: extra)
+}
